8a796e2a91ca66ed9afa35b741e819794ad154c3
Amanda Brown <schoolemail4635@gmail.com>
2024-02-27 17:56:06 -0500

    data models

---
+++ src/main/java/cis/javaholics/models/Likes.java
@@ -0,0 +1,24 @@
+package cis.javaholics.models;
+
+import com.google.cloud.Timestamp;
+import com.google.cloud.firestore.annotation.DocumentId;
+import com.google.protobuf.util.Timestamps;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+import java.text.ParseException;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Likes {
+    @DocumentId
+    private @Nullable String likeId;
+    private String senderId;
+    private String receiverId;
+    private Timestamp time;
+
+
+}
---
+++ src/main/java/cis/javaholics/models/forumPosts/AForumPosts.java
@@ -0,0 +1,30 @@
+package cis.javaholics.models.forumPosts;
+
+import cis.javaholics.models.Likes;
+import com.google.cloud.Timestamp;
+import com.google.cloud.firestore.annotation.DocumentId;
+import com.google.protobuf.util.Timestamps;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+import java.text.ParseException;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public abstract class AForumPosts {
+    @DocumentId
+    private @Nullable String fPostId;
+    private String topic;
+    private String description;
+    private String title;
+    private List<String> photos;
+    private Timestamp postedAt;
+
+    public void setPostedAt(String postedAt) throws ParseException {
+        this.postedAt = Timestamp.fromProto(Timestamps.parse(postedAt));
+    }
+}
